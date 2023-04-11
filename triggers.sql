986628ff37010612953291250eb6e185fd2f9fed
viksit-siddhant <s.r.viksit@gmail.com>
2023-04-11 17:29:45 +0530

    Cringe commit

--- triggers.sql
+++ triggers.sql
@@ -1,7 +1,9 @@
-CREATE TRIGGER delete_cart_items AFTER INSERT ON orders
+-- Create trigger to delete cart items of user when user places an order
+
+CREATE TRIGGER delete_cart_itemsv2 AFTER DELETE ON orders
 FOR EACH ROW
     DELETE FROM carts
-    WHERE orderid = NEW.orderid;
+    WHERE orderid = OLD.orderid;
 
 
 CREATE TRIGGER delete_seller_products AFTER DELETE ON sellers
